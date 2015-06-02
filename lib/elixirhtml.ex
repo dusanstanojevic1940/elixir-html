ba7401a928e87267c057caf96d6b38cec7d733fe
Dusan Stanojevic <dusan.stanojevic.1940@metropolitan.ac.rs>
2015-06-03 00:25:12 +0200

    Added most of method declarations

--- lib/elixirhtml.ex
+++ lib/elixirhtml.ex
@@ -1,2 +1,60 @@
 defmodule ElixirHtml do
+  @moduledoc """
+  Contains HTML data minning functions
+  """
+
+  def valid_html?(text) do 
+	  @doc """
+	  Returns if text is a valid HTML text
+	  """
+
+  end
+  
+  def nth_element_from_element(element) do
+  end
+
+  def elements_from_element do
+  end
+
+  def elements_from_text(text) do
+  	@doc """
+  	
+  	"""
+  end
+
+  def html_element_from_text(text) do
+  	@doc """
+  	
+  	"""
+  end
+
+  def text_of_element(element) do doc
+  	@doc """
+  	
+  	"""
+  end
+
+  def element_by_id(id) do
+  	@doc """
+  	
+  	"""
+  end
+
+  def elements_by_selector(selector) do
+  	@doc """
+  	
+  	"""
+  end
+
+  def elements_by_attr_value(attr, value) do
+  	@doc """
+  	
+  	"""
+  end
+
+  def elements_by_tag(tag) do
+    @doc """
+    
+  	"""
+  end
 end
